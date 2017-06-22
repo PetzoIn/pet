175e34d92bc17a5c57ab4fb4c6b2d5bc8377e3a5
rajat2797 <rajatsinghal1997@gmail.com>
2017-06-22 12:55:50 +0530

    template change

--- frobshop/petzoapp/views.py
+++ frobshop/petzoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from models import *
 
 def index(request):
 	return HttpResponse('Main App')
