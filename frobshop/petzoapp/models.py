287144878ed41281d87647d2d0793da6f8efeb9e
rajat2797 <rajatsinghal1997@gmail.com>
2017-07-11 12:13:57 +0530

    deleted pastfoodModel

--- frobshop/petzoapp/models.py
+++ frobshop/petzoapp/models.py
@@ -25,18 +25,11 @@ class Dog(models.Model):
 	weight = models.IntegerField(blank=False)
 	bodyCondition = models.CharField(blank=False, max_length=128)
 	activity = models.CharField(blank=False, max_length=128)
+	pastfood = models.CharField(blank=True, max_length=128)
 	image = models.ImageField(upload_to='media/cache', default='static/images/dogDefault.png')
 
 	def __unicode__(self):
 		return str(self.id)
-
-class PastFoodDog(models.Model):
-	dog = models.ForeignKey(Dog)
-	food = models.TextField(blank=False)
-	timestamp = models.DateTimeField(auto_now_add=True)
-
-	def __unicode__(self):
-		return str(self.id) + food
 
 class ReferralCode(models.Model):
 	user = models.ForeignKey(User)
